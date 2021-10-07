cdec54335daaa5da5f3cc860d6f07b59f90fb03b
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2021-10-06 21:24:57 -0400

    Aplicação finalizada.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
+import Modal from 'react-modal';
 
-function App() {
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
+export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TransactionsProvider>
+    {/* Repasse de propriedades */}
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
+      <GlobalStyle />
+    </TransactionsProvider>
   );
 }
 
-export default App;
+/* o export default permite que na importação seja mudado o nome do componente. 
+  Já o export sem o default, não permite, o nome do componente ao ser importado precisa
+  ser igual ao mesmo nome de exportação.
+*/
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { TransactionsTable } from '../TransactionsTable';
+import { Summary } from '../Summary';
+
+import { Container } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
+}
